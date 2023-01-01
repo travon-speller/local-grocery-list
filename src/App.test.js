ef5d9d168d3bee75a558adf4e2e6914f437489a7
Travon <travonspeller@gmail.com>
2023-01-01 14:36:26 -0500

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders grocery list', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const linkElement = screen.getByText(/grocery list/i);
   expect(linkElement).toBeInTheDocument();
 });
